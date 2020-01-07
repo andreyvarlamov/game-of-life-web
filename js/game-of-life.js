3f280ef637e866e1a2f64575fe10652117b4f34e
andreyvarlamov <andreyvarlamov@github.com>
2020-01-07 05:56:06 -0800

    Game of Life algorithm implemented

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -11,7 +11,8 @@ function Table(size, cell_size) {
 
         for (i = 0; i < this.rows.length; i++) {
             for (j = 0; j < this.rows[0].length; j++) {
-                this.rows[i][j] = Math.round(Math.random());
+                randStatus = Math.round(Math.random())
+                this.rows[i][j] = [randStatus, randStatus];
             }
         }
     }
@@ -19,7 +20,7 @@ function Table(size, cell_size) {
     this.redraw = function() {
         for (i = 0; i < this.rows.length; i++) {
             for (j = 0; j < this.rows[0].length; j++) {
-                if (this.rows[i][j] == 1) {
+                if (this.rows[i][j][0] == 1) {
                     fill('blue');
                 }
                 else {
@@ -27,6 +28,68 @@ function Table(size, cell_size) {
                 }
 
                 rect(i*cell_size, j*cell_size, (i+1)*cell_size, (j+1)*cell_size);
+            }
+        }
+    }
+
+    this.countNeighbors = function(x, y) {
+        var neighborCount = 0;
+        var neighborOffsets = [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1], [0,-1], [1,-1]];
+
+        var that = this;
+
+        var count = function(offset, index) {
+            try {
+                var i = x+offset[0]
+                var j = y+offset[1]
+                var b = that.rows[i][j][0] == 1
+                if (b) {
+                    neighborCount++;
+                }
+            }
+            catch {}
+        }
+
+        neighborOffsets.forEach(count);
+
+        return neighborCount;
+
+    }
+
+    this.checkCell = function(x, y) {
+        var neighborCount = this.countNeighbors(x, y);
+
+        if (this.rows[x][y][0] == 1) {
+            if (neighborCount < 2 || neighborCount > 3) {
+                this.rows[x][y][1] = 0;
+            }
+            else {
+                this.rows[x][y][1] = 1;
+            }
+        }
+        else {
+            if (neighborCount == 3) {
+                this.rows[x][y][1] = 1;
+            }
+            else {
+                this.rows[x][y][1] = 0;
+            }
+        }
+    }
+
+    this.processCell = function(x, y) {
+        this.rows[x][y][0] = this.rows[x][y][1];
+    }
+
+    this.process = function() {
+        for(i=0; i<this.size; i++) {
+            for (j=0; j<this.size; j++) {
+                this.checkCell(i, j);
+            }
+        }
+        for(i=0; i<this.size; i++) {
+            for (j=0; j<this.size; j++) {
+                this.processCell(i, j);
             }
         }
     }
--- js/sketch.js
+++ js/sketch.js
@@ -3,12 +3,13 @@ function setup() {
     cell_size = 10;
     createCanvas(size*cell_size+1, size*cell_size+1);
     t = new Table(size, cell_size);
-    
+    t.generateCells();
     frameRate(5);
   } 
   
   function draw() {
-    t.generateCells();
+    
+    t.process();
     t.redraw();
     
   }

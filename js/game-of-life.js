188a0ea75126ae46ad05e6777bd881193bc337fc
andreyvarlamov <varlamov.and@gmail.com>
2020-01-12 17:18:11 -0800

    Refactoring

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -1,76 +1,71 @@
 function Table(size, cell_size) {
-    this.size = size;
-    this.grid = new Array(this.size);
-    this.nextGrid = new Array(this.size);
-    this.cell_size = cell_size;
-    
+  this.size = size;
+  this.grid = new Array(this.size);
+  this.nextGrid = new Array(this.size);
+  this.cell_size = cell_size;
 
-    this.generateCells = function() {
-        for (i = 0; i < this.grid.length; i++) {
-            this.grid[i] = new Array(this.size);
-            this.nextGrid[i] = new Array(this.size);
-        }
-
-        for (i = 0; i < this.size; i++) {
-            for (j = 0; j < this.size; j++) {
-                if (i == 0 || j == 0 || i == this.size - 1 || j == this.size - 1) this.grid[i][j] = 0;
-                else this.grid[i][j] = Math.round(Math.random());
-
-                this.nextGrid[i][j] = 0;
-            }
-        }
+  this.generateCells = function() {
+    for (i = 0; i < this.grid.length; i++) {
+      this.grid[i] = new Array(this.size);
+      this.nextGrid[i] = new Array(this.size);
     }
 
-    this.redraw = function() {
-        for (i = 0; i < this.size; i++) {
-            for (j = 0; j < this.size; j++) {
-                if (this.grid[i][j] == 1) fill('#AA0F0F');
-                else fill('#FFFFFF');
+    for (i = 0; i < this.size; i++) {
+      for (j = 0; j < this.size; j++) {
+        if (i == 0 || j == 0 || i == this.size - 1 || j == this.size - 1)
+          this.grid[i][j] = 0;
+        else this.grid[i][j] = Math.round(Math.random());
 
-                stroke('#CCCCCA');
-                strokeWeight(1);
-
-                rect(i*cell_size, j*cell_size, cell_size-1, cell_size-1);
-            }
-        }
+        this.nextGrid[i][j] = 0;
+      }
     }
+  };
 
-    this.process = function() {
-        for(let x = 1; x < this.size - 1; x++) {
-            for(let y = 1; y < this.size - 1; y++) {
-                let neighborCount = 0;
+  this.redraw = function() {
+    for (i = 0; i < this.size; i++) {
+      for (j = 0; j < this.size; j++) {
+        if (this.grid[i][j] == 1) fill("#AA0F0F");
+        else fill("#FFFFFF");
 
-                for (let i = -1; i <= 1; i++) {
-                    for (let j = -1; j <= 1; j++) {
-                        neighborCount += this.grid[x+i][y+j];
-                    }    
-                }
+        stroke("#CCCCCA");
+        strokeWeight(1);
 
-                neighborCount -= this.grid[x][y]
+        rect(i * cell_size, j * cell_size, cell_size - 1, cell_size - 1);
+      }
+    }
+  };
 
-                if (this.grid[x][y] == 1) {
-                    if (neighborCount < 2 || neighborCount > 3) {
-                        this.nextGrid[x][y] = 0;
-                    }
-                    else {
-                        this.nextGrid[x][y] = 1;
-                    }
-                }
-                else {
-                    if (neighborCount == 3) {
-                        this.nextGrid[x][y] = 1;
-                    }
-                    else {
-                        this.nextGrid[x][y] = 0;
-                    }
-                }
+  this.process = function() {
+    for (let x = 1; x < this.size - 1; x++) {
+      for (let y = 1; y < this.size - 1; y++) {
+        let neighborCount = 0;
 
-                
-            }
+        for (let i = -1; i <= 1; i++) {
+          for (let j = -1; j <= 1; j++) {
+            neighborCount += this.grid[x + i][y + j];
+          }
         }
 
-        let temp = this.grid;
-        this.grid = this.nextGrid;
-        this.nextGrid = temp;
+        neighborCount -= this.grid[x][y];
+
+        if (this.grid[x][y] == 1) {
+          if (neighborCount < 2 || neighborCount > 3) {
+            this.nextGrid[x][y] = 0;
+          } else {
+            this.nextGrid[x][y] = 1;
+          }
+        } else {
+          if (neighborCount == 3) {
+            this.nextGrid[x][y] = 1;
+          } else {
+            this.nextGrid[x][y] = 0;
+          }
+        }
+      }
     }
+
+    let temp = this.grid;
+    this.grid = this.nextGrid;
+    this.nextGrid = temp;
+  };
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,14 @@
-function setup() { 
-    size = 30;
-    cell_size = 17;
-    let canvas = createCanvas(size*cell_size, size*cell_size);
-    canvas.parent('canvas-div');
-    t = new Table(size, cell_size);
-    t.generateCells();
-    frameRate(30);
-  } 
-  
-  function draw() {
-    if (state == 1) t.process();
-    t.redraw();
-  }
+function setup() {
+  size = 30;
+  cell_size = 17;
+  let canvas = createCanvas(size * cell_size, size * cell_size);
+  canvas.parent("canvas-div");
+  t = new Table(size, cell_size);
+  t.generateCells();
+  frameRate(30);
+}
+
+function draw() {
+  if (state == 1) t.process();
+  t.redraw();
+}
